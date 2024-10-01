1f17a04d84eb39a5bcc7fdb4c67a53755e2b40c3
thomas <thomas.chu@techdesign.com>
2024-10-01 10:25:44 +0800

    update cors

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 var express = require("express");
 var router = express.Router();
+var cors = require("cors"); // 引入 cors
+
+var corsOptions = {
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200, // 一些旧版浏览器 (如IE11) 可能会对204状态码有问题
+};
+
+router.use(cors(corsOptions)); // 使用 cors 中间件
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
